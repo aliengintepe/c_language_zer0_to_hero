5cc2393d764761331e24c38118615ca0d908c155
aliengintepe <119291096+aliengintepe@users.noreply.github.com>
2022-12-11 03:35:38 +0300

    ali engintepe
    @themanoftalent

--- Cozumler/Iki_sayi.c
+++ Cozumler/Iki_sayi.c
@@ -2,14 +2,14 @@
 
 int main()
 {    
- int x = 0, y = 0,sonuc=0;  //Onceden deklare ettim. burada int veri tipi, x y degisken 
+ int x = 0, y = 0,result=0;  //Onceden deklare ettim. burada int veri tipi, x y degisken 
   
 printf("Enter first number:");   //yazar
 scanf("%d",&x);  
 printf("Enter second number:");  //input alir
 scanf("%d",&y);  
   
-sonuc=x+y;  
+result=x+y;  
 printf("sum of 2 numbers:%d ",result);  
   
 return 0;  
