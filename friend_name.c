e6a73a6c77dbf682a4b5deb9fe35f02a6fbbf48b
aliengintepe <119291096+aliengintepe@users.noreply.github.com>
2022-12-11 03:30:37 +0300

    Ali ENGİNTEPE
    @themanoftalent

--- friend_name.c
+++ friend_name.c
@@ -3,3 +3,19 @@
 // This is my bff. 
 
 // Good Luck
+#include <stdio.h>
+#include<string.h>
+
+
+int main()
+{
+    char name[50];
+    printf("enter a name :");
+    scanf("%s",&name);
+    if(strcmp(name,"ali")==0){
+        printf("it is me");
+    }
+    else{
+        printf("%s is my best friend :",name);
+    }
+}
